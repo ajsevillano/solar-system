d0c11f8403a6828722605c6c5a32ce15b13bcbce
Sevi <correosevi@gmail.com>
2020-09-16 16:04:23 +0100

    first commit :tada:

---
+++ src/pages/planets.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Planets() {
+	return (
+		<div className="page">
+			<h1>Planets</h1>
+			<p>This is the planets page.</p>
+		</div>
+	);
+}
+
+export default Planets;
