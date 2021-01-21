4e699ed8b84c5fb64a618acb9267931d33807e65
Sevi <correosevi@gmail.com>
2021-01-21 11:19:15 +0000

    Refactory card and App background

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -7,99 +7,99 @@ import Home from '../../pages/home';
 import Planets from '../../pages/planets';
 
 function Navsticky() {
-	return (
-		<Router>
-			<nav className="botton-menu">
-				<div className="menu-buttons">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="icon icon-tabler icon-tabler-home"
-						width="32"
-						height="32"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="#2c3e50"
-						fill="none"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" />
-						<polyline points="5 12 3 12 12 3 21 12 19 12" />
-						<path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-						<path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-					</svg>
-					<p>
-						<Link to="/">Home</Link>
-					</p>
-				</div>
-				<div className="menu-buttons">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="icon icon-tabler icon-tabler-planet"
-						width="32"
-						height="32"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="#2c3e50"
-						fill="none"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" />
-						<path d="M17.905 13.057c2.208 1.756 3.436 3.308 3.012 4.035-.67 1.146-5.204-.204-10.129-3.016-4.924-2.812-8.374-6.022-7.705-7.168.418-.716 2.347-.458 4.936.524" />
-						<circle cx="12" cy="12" r="6" />
-					</svg>
-					<p>
-						<Link to="/planets">Planets</Link>
-					</p>
-				</div>
-				<div className="menu-buttons">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="icon icon-tabler icon-tabler-news"
-						width="32"
-						height="32"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="#2c3e50"
-						fill="none"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" />
-						<path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" />
-						<line x1="8" y1="8" x2="12" y2="8" />
-						<line x1="8" y1="12" x2="12" y2="12" />
-						<line x1="8" y1="16" x2="12" y2="16" />
-					</svg>
-					<p>news</p>
-				</div>
-				<div className="menu-buttons">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="icon icon-tabler icon-tabler-settings"
-						width="32"
-						height="32"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="#2c3e50"
-						fill="none"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" />
-						<path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-						<circle cx="12" cy="12" r="3" />
-					</svg>
-					<p>settings</p>
-				</div>
-			</nav>
-			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/planets" component={Planets} />
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <nav className="botton-menu">
+        <div className="menu-buttons">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-home"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <polyline points="5 12 3 12 12 3 21 12 19 12" />
+            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+          </svg>
+          <p>
+            <Link to="/">Home</Link>
+          </p>
+        </div>
+        <div className="menu-buttons">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-planet"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path d="M17.905 13.057c2.208 1.756 3.436 3.308 3.012 4.035-.67 1.146-5.204-.204-10.129-3.016-4.924-2.812-8.374-6.022-7.705-7.168.418-.716 2.347-.458 4.936.524" />
+            <circle cx="12" cy="12" r="6" />
+          </svg>
+          <p>
+            <Link to="/planets">Planets</Link>
+          </p>
+        </div>
+        <div className="menu-buttons">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-news"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" />
+            <line x1="8" y1="8" x2="12" y2="8" />
+            <line x1="8" y1="12" x2="12" y2="12" />
+            <line x1="8" y1="16" x2="12" y2="16" />
+          </svg>
+          <p>news</p>
+        </div>
+        <div className="menu-buttons">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-settings"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+            <circle cx="12" cy="12" r="3" />
+          </svg>
+          <p>settings</p>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/planets" component={Planets} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default Navsticky;
