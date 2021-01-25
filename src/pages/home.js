269c11f65d7f86ada1673fc3ebb1589d810f97de
Sevi <correosevi@gmail.com>
2021-01-25 20:29:04 +0000

    Encapsulate the map inside a function

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Scroll from '../components/home/Scroll';
 
 const Home = ({ title }) => {
+  const HandlePlanetData = () => {
+    return title.map((home) => <div key={home.id}>{home.name}</div>);
+  };
+
   return (
     <div className="page">
-      {title.map((home) => (
-        <div key={home.id}>{home.name}</div>
-      ))}
+      {HandlePlanetData()}
       <Scroll />
     </div>
   );
