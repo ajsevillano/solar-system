15832a076b796a4561329ef8bb9e3df4f2160c8d
Sevi <correosevi@gmail.com>
2021-01-26 20:20:00 +0000

    Add a filter in planetHScrolling to return only the planets

--- src/components/home/planetsHScrolling.js
+++ src/components/home/planetsHScrolling.js
@@ -1,8 +1,13 @@
 import React from 'react';
 const planetsHScrolling = ({ planetsData }) => {
-  //Create the card with the Planet information passed in the prop
   const HandlePlanetData = () => {
-    return planetsData.map((planetCard) => (
+    //Filter only the planets
+    let PlanetsFilter = planetsData.filter(
+      (planets) => planets.type === 'planet'
+    );
+
+    //Create the card with the Planet information from the filter
+    return PlanetsFilter.map((planetCard) => (
       <div className="scroll_card" key={planetCard.id}>
         <img className="scroll_planet" src={planetCard.imgPath} alt="" />
         <h2>{planetCard.name}</h2>
@@ -11,7 +16,7 @@ const planetsHScrolling = ({ planetsData }) => {
   };
 
   return (
-  <section id="planets">
+    <section id="planets">
       <h1>The planets</h1>
       <div className="subheaders">
         <h3>The 8 planets </h3>
