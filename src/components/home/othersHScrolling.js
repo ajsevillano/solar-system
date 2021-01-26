1d854b15f74e37f097c6bfd3b0603aebd754535a
Sevi <correosevi@gmail.com>
2021-01-26 14:01:44 +0000

    update manifest.json  and logos

---
+++ src/components/home/othersHScrolling.js
@@ -0,0 +1,39 @@
+import React from 'react';
+const othersHScrolling = ({ planetsData }) => {
+  //Create the card with the Planet information passed in the prop
+  const HandlePlanetData = () => {
+    return planetsData.map((planetCard) => (
+      <div className="scroll_card" key={planetCard.id}>
+        <img className="scroll_planet" src={planetCard.imgPath} alt="" />
+        <h2>{planetCard.name}</h2>
+      </div>
+    ));
+  };
+
+  return (
+   <div className="scroll_container">
+      <h1>Others</h1>
+
+      <div className="subheaders">
+        <h3>Another objects in the solar system</h3>
+        <p>SEE ALL</p>
+      </div>
+      <div className="scroll_cards_container">
+        <div className="scroll_card">
+          <img className="scroll_planet" src="./img/mercury.png" alt="" />
+          <h2>Moons</h2>
+        </div>
+        <div className="scroll_card">
+          <img className="scroll_planet" src="./img/venus.png" alt="" />
+          <h2>Asteroids</h2>
+        </div>
+        <div className="scroll_card">
+          <img className="scroll_planet" src="./img/earth.png" alt="" />
+          <h2>Comets</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default othersHScrolling;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Planetsscroll from '../components/home/planetsHScrolling';
+import Otherssscroll from '../components/home/othersHScrolling';
 
 const Home = ({ planetsData }) => {
   return (
     <div className="scroll_container">
       <Planetsscroll planetsData={planetsData} />
+      <Otherssscroll planetsData={planetsData}/>
     </div>
   );
 };
