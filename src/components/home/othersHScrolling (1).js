196958ac051419794c08eace0002278014d7cab8
Sevi <correosevi@gmail.com>
2021-01-27 10:50:09 +0000

    Add script for auto git

---
+++ src/components/home/othersHScrolling (1).js
@@ -0,0 +1,25 @@
+import React from 'react';
+const otherHScrolling = ({ planetsData }) => {
+  //Create the card with the Planet information passed in the prop
+  const HandlePlanetData = () => {
+    return planetsData.map((planetCard) => (
+      <div className="scroll_card" key={planetCard.id}>
+        <img className="scroll_planet" src={planetCard.imgPath} alt="" />
+        <h2>{planetCard.name}</h2>
+      </div>
+    ));
+  };
+
+  return (
+    <React.Fragment>
+      <h1>The planets</h1>
+      <div className="subheaders">
+        <h3>The 8 planets </h3>
+        <p>SEE ALL</p>
+      </div>
+      <div className="scroll_cards_container">{HandlePlanetData()}</div>
+    </React.Fragment>
+  );
+};
+
+export default planetsHScrolling;
